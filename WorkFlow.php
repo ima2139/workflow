4d6a57c34a9338374a95ff806feb3738ea6e97a2
cnb5597 <cnb5597@rit.edu>
2020-01-29 11:35:05 -0500

    Few Changes

--- WorkFlow.php
+++ WorkFlow.php
@@ -46,7 +46,7 @@
                         </div>
                         
                         <div>
-                            <a href="#" id="myBtn" style="margin-bottom: 10px;">Forget Password / Request Reset?</a> <br>
+                            <a href="#" id="myBtn" style="margin-bottom: 10px;">Forget Password?</a> <br>
                             <button class="btn btn--radius-2 btn--blue submit_btn" type="submit">Submit</button>
 
                           
@@ -81,7 +81,7 @@
         
         
         <div>
-            <button class="btn btn--radius-2 btn--green submit_btn" type="submit">Submit</button>
+            <button class="btn btn--radius-2 btn--green submit_btn" type="submit">Login</button>
         </div>
     </form>
   </div>
